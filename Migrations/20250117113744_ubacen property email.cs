efc42722b9261036355ddc76162e43cdcfa1fc67
markofilipovic2762 <markofilipovicsd@gmail.com>
2025-01-17 15:19:27 +0100

    17.1

---
+++ Migrations/20250117113744_ubacen property email.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShopAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ubacenpropertyemail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Suppliers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Suppliers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Suppliers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Shippers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Shippers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Suppliers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
