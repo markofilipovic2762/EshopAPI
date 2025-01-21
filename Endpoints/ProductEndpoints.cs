4b02f84d34357418b446347f6b62acbe71afde88
markofilipovic2762 <markofilipovicsd@gmail.com>
2025-01-21 15:28:05 +0100

    21.1 image upload product

--- Endpoints/ProductEndpoints.cs
+++ Endpoints/ProductEndpoints.cs
@@ -1,5 +1,6 @@
 using EShopAPI.Interfaces;
 using EShopAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EShopAPI.Endpoints;
@@ -23,6 +24,33 @@ public static class ProductEndpoints
             async ([FromBody] Product product, IProductRepository products) =>
             await products.UpdateProductAsync(product));
         group.MapDelete("/{id:int}", async (int id, IProductRepository products) => await products.DeleteProductAsync(id));
+        group.MapPost("/upload", async (IFormFile file) =>
+        {
+            if (file == null || file.Length == 0)
+            {
+                return Results.BadRequest("Invalid file.");
+            }
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+            var fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+            if (!allowedExtensions.Contains(fileExtension))
+            {
+                return Results.BadRequest("File type not supported.");
+            }
+
+            // Čuvanje fajla u lokalni fajl sistem ili cloud
+            var fileName = Guid.NewGuid() + fileExtension;
+            var filePath = Path.Combine("Uploads", fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            
+            var fileUrl = $"http://localhost:5131/uploads/{fileName}";
+
+            return Results.Ok(new { Url = fileUrl });
+        });
         return group;
     }
 }
